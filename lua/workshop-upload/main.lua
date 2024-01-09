89602b6f459f85aa4b8a1d57836ac137ad550be5
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2024-01-09 01:26:20 -0300

    Added basic git version checker code

--- lua/workshop-upload/main.lua
+++ lua/workshop-upload/main.lua
@@ -1,6 +1,7 @@
 local file   = file
 local string = string
 local Target = "addons/%s/lua/workshop-upload/main.lua"
+local Head   = "ref: refs/heads/(%w+)"
 
 local function GetRootFolder()
     local _, Folders = file.Find("addons/*", "GAME")
@@ -13,3 +14,23 @@ local function GetRootFolder()
         end
     end
 end
+
+local function GetBranch(Path)
+    local Content = file.Read(Path .. "/.git/HEAD", "GAME")
+
+    return string.match(Content, Head) -- NOTE: Will fail if the branch has any non-alphanumerical characters!
+end
+
+local function GetLatestCommit(Path, Branch)
+    return file.Read(Path .. "/.git/refs/heads/" .. Branch, "GAME")
+end
+
+local function PrintVersion()
+    local Path   = GetRootFolder()
+    local Branch = GetBranch(Path)
+    local Commit = GetLatestCommit(Path, Branch)
+
+    print(Branch .. "-" .. string.sub(Commit, 1, 6))
+end
+
+PrintVersion()
